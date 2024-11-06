717b38893a11c2daba15364bac9b427bd35d53eb
nomisaur <naomisaltis@gmail.com>
2024-11-05 21:21:03 -0800

    typo

--- app/gungirl/chapters/chapter3.tsx
+++ app/gungirl/chapters/chapter3.tsx
@@ -7,13 +7,13 @@ export const Chapter3 = () => {
       <p>
         You stood outside your parents home looking into the woods beyond. You
         lived on the threshhold of a small community of houses spread out among
-        the tall coastal trees. To the East things became more urban as the land
-        became flatter and closer to the bay. To the West was an expanse of
-        trees and mist and hills that rose higher up until they peaked and then
-        fell again into the vast Pacific Ocean.
+        the tall coastal trees. To the East things become more urban as the land
+        becomes flatter and closer to the bay. To the West is an expanse of
+        trees and mist and hills that rise higher up until they peak and then
+        fall again into the vast Pacific Ocean.
       </p>
       <p>
-        You were not aloud past the treeline, and yet there you stood carefully
+        You were not alowed past the treeline, and yet there you stood carefully
         considering whether or not to become a rule-breaker. This might be the
         first rule that you would be consciously breaking. You looked back
         towards your home. The more you stared at it the more it narrowed in on
