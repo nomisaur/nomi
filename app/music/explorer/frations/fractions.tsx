65b366c5d9996cb2f940b42142dc143262a939f4
nomisaur <naomisaltis@gmail.com>
2025-02-06 22:14:26 -0800

    buildable

--- app/music/explorer/frations/fractions.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles.module.css";
-
-export const Fraction = ({ top = 1, bottom = 1 }) => (
-  <div className={styles.frac}>
-    <span className={styles.top}>{top}</span>
-    <span className={styles.symbol}>/</span>
-    <span className={styles.bottom}>{bottom}</span>
-  </div>
-);
